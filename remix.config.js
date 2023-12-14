69965be5d7c4240fb5a4356c1f8e94a3e04329c3
Georg Ekeberg <georghagen@gmail.com>
2023-12-14 14:44:46 +0100

    attempt with dockerfile

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
+  cacheDirectory: "./node_modules/.cache/remix",
+  ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/public",
