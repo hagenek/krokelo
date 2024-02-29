b79bfb1e968e48d680246fceb3b1f2c95eba31d3
Steffen Ekeberg Bråten <steffen.ekeberg.braaten@sparebank1.no>
2024-02-29 14:56:45 +0100

    d

--- app/routes/compare-players.tsx
+++ app/routes/compare-players.tsx
@@ -2,9 +2,10 @@
 import { type MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { useState } from 'react';
-import { Player, getPlayers } from '../services/player-service';
+import {Player, getPlayers, RecentMatch} from '../services/player-service';
 import GenericSearchableDropdown from '~/ui/searchable-dropdown';
 import { Jsonify } from '@remix-run/server-runtime/dist/jsonify';
+import { Player as PrismaPlayer} from "@prisma/client";
 
 export const meta: MetaFunction = () => {
   return [
@@ -25,14 +26,44 @@ export const loader = async () => {
   return { players };
 };
 
+const getEloForPlayerAfterMatch = (
+    player: PrismaPlayer,
+    match: Jsonify<RecentMatch>,
+    players: Jsonify<Player[]>
+) => {
+  const enrichedPlayer = players.find((p) => p.id === player.id);
+  if (!enrichedPlayer) {
+    console.error(`Player ${player.name} not found in enriched player list`);
+    return 0;
+  }
+
+  const matchEloIndex = enrichedPlayer.eloLogs.findIndex(
+      (log) => match.id === log.matchId
+  );
+  // No match played
+  if (matchEloIndex === -1) {
+    return 1500;
+  }
+
+  return enrichedPlayer.eloLogs[matchEloIndex].elo;
+};
+
 const calculateEloExchangeBetweenPlayers = (
   player1: Jsonify<Player>,
   player2: Jsonify<Player>
 ) => {
-  const player1Matches = player1.matchesAsWinner + player1.matchesAsLoser;
+  const player1Matches = player1.matchesAsWinner.concat(player1.matchesAsLoser);
+  const player2Matches = player2.matchesAsWinner.concat(player2.matchesAsLoser);
+  console.log('player1Matches', player1Matches);
+    console.log('player2Matches', player2Matches);
   const allMatchesBetweenPlayers = player1Matches.filter(
     (match) => match.winnerId === player2.id || match.loserId === player2.id
   );
+
+  console.log(player1)
+
+    console.log('allMatchesBetweenPlayers', allMatchesBetweenPlayers);
+    ;
 
   // const player1Elo = player1.eloLogs[player1Matches - 1].elo;
   // const player2Elo = player2.eloLogs[player2Matches - 1].elo;
@@ -82,6 +113,7 @@ export default function Index() {
   };
 
   console.log('players', players);
+  console.log("difference", player2 && player1 ?calculateEloExchangeBetweenPlayers(player1, player2) : "no players selected")
 
   return (
     <>
